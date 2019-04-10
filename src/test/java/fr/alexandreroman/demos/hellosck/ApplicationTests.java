8eee9f91c9717286ff19b4a1dd040fa4de5421b5
Alexandre Roman <aroman@pivotal.io>
2019-04-10 22:26:03 +0200

    Add unit tests

--- src/test/java/fr/alexandreroman/demos/hellosck/ApplicationTests.java
+++ src/test/java/fr/alexandreroman/demos/hellosck/ApplicationTests.java
@@ -1,16 +1,48 @@
+/*
+ * Copyright (c) 2019 Pivotal Software, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package fr.alexandreroman.demos.hellosck;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ApplicationTests {
+    @Autowired
+    private MockMvc mvc;
 
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void testHello() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN))
+                .andExpect(content().string("Hello world!"));
+    }
 }
